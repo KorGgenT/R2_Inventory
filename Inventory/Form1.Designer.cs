b4324a84d37869ead1762cd32fd55c1709487ab9
KorGgenT <curtis.r.merrill@gmail.com>
2018-07-12 12:54:21 -0400

    initial commit
    Fully working. Supposedly.

---
+++ Inventory/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Inventory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Asset = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Checked = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Description,
+            this.Asset,
+            this.Checked});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1270, 494);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Description";
+            this.Description.Name = "Description";
+            this.Description.Width = 450;
+            // 
+            // Asset
+            // 
+            this.Asset.HeaderText = "Asset";
+            this.Asset.Name = "Asset";
+            // 
+            // Checked
+            // 
+            this.Checked.HeaderText = "X";
+            this.Checked.Name = "Checked";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Enter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(406, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(512, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 31);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Print";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            this.printPreviewDialog1.Load += new System.EventHandler(this.printPreviewDialog1_Load);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1311, 556);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Asset;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Checked;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
+
---
+++ Inventory/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Inventory
+{
+    public partial class Form1 : Form
+    {
+
+        public string[][] LoadList(string filePath)
+        {
+
+
+            StreamReader sr = new StreamReader(@filePath);
+            var lines = new List<string[]>();
+            int Row = 0;
+            while (!sr.EndOfStream)
+            {
+                string[] Line = sr.ReadLine().Split('\t');
+                lines.Add(Line);
+                Row++;
+                //Console.WriteLine(Row);
+            }
+            return lines.ToArray();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // LOAD FILE
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog1.Filter = "Excel files (*.xlsx)|*.xlsx";
+            //openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                System.IO.FileInfo fInfo = new System.IO.FileInfo(openFileDialog1.FileName);
+
+                string strFileName = fInfo.Name;
+                string strFilePath = fInfo.DirectoryName;
+                string path = strFilePath + "\\" + strFileName;
+               
+                try
+                {
+                    var workbook = new XLWorkbook(path);
+                    var worksheet = workbook.Worksheet(1);
+                    int new_input = 0;
+                    int last_row = worksheet.LastRowUsed().RowNumber();
+
+                    if ((string)worksheet.Cell("A1").Value == "DESCRIPTION")
+                    {
+                        new_input = 1;
+                    }
+
+                    for (int i = 1 + new_input; i < last_row; i++)
+                    {
+                        // Cell "A" is Description, Cell "B" is asset, Cell "C" is check.
+                        bool check = false;
+                        var column_c = worksheet.Cell("C" + i).Value;
+                        if (Object.ReferenceEquals(column_c.GetType(), "bool"))
+                        {
+                            check = true;
+                        }
+
+                        if ((string)worksheet.Cell("A" + i).Value != "")
+                        {
+                            dataGridView1.Rows.Add((string)worksheet.Cell("A" + i).Value, worksheet.Cell("B" + i).Value, check);
+                        }
+                    }
+
+                    dataGridView1.Refresh();
+
+                    return;
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("File open in another window. Please close and try again.");
+                    return;
+                }
+                
+
+                
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) // DATA ENTRY
+        {
+
+        }
+        private void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+            simpleSound.Play();
+        }
+        private void playSimpleSound2()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chord.wav");
+            simpleSound.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // ENTER DATA BUTTON
+        {
+            string search = textBox1.Text;
+            if (search != "")
+            {
+                //MessageBox.Show(search);
+                bool found = false;
+                int size = dataGridView1.RowCount;
+                int i = 0;
+                //MessageBox.Show(size.ToString());
+                while (!found && i < size)
+                {
+                    if ((string)dataGridView1.Rows[i].Cells[1].Value == search)
+                    {
+                        found = true;
+                        dataGridView1.Rows[i].Cells[2].Value = true;
+                    }
+                    i++;
+                }
+                if (!found)
+                {
+                    playSimpleSound2();
+                    MessageBox.Show("Not found");
+                }
+                else
+                { playSimpleSound(); }
+                textBox1.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) // SAVE FILE
+        {
+            saveFile(false);
+        }
+
+        private void button4_Click_1(object sender, EventArgs e) // "PRINT" (meaning save, then open in windows)
+        {
+            saveFile(true);
+        }
+
+        private void saveFile(bool openFile)
+        {
+            // Displays a SaveFileDialog so the user can save the file
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "XLSX File|*.xlsx";
+            saveFileDialog1.Title = "Save your inventory";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the file via a FileStream created by the OpenFile method.  
+                System.IO.FileInfo fInfo = new System.IO.FileInfo(saveFileDialog1.FileName);
+
+                string path = fInfo.DirectoryName + "\\" + fInfo.Name;
+
+                var workbook = new XLWorkbook();
+                var worksheet = workbook.Worksheets.Add("Serial Inventory");
+                int row_size = dataGridView1.RowCount;
+                for (int i = 0; i < row_size - 1; i++)
+                {
+                    // there are only 3 columns: description, asset, and true/false (which is simply to see if it is checked)
+                    worksheet.Cell("A" + (i + 1)).Value = dataGridView1.Rows[i].Cells[0].Value;
+                    worksheet.Cell("B" + (i + 1)).Value = dataGridView1.Rows[i].Cells[1].Value;
+                    worksheet.Cell("C" + (i + 1)).Value = dataGridView1.Rows[i].Cells[2].Value;
+                }
+                worksheet.Columns().AdjustToContents();
+
+                path = Regex.Replace(path, ".xlsx", "");
+
+                workbook.SaveAs(path + ".xlsx"); // this does NOT correctly interpret filenames that DO contain ".xlsx"
+                if (openFile)
+                {
+                    Process process = new Process();
+                    Process.Start("Excel.exe", path);
+                }
+            }
+        }
+
+        private void printPreviewDialog1_Load(object sender, EventArgs e)
+        {
+            ShowDialog();
+        }
+
+        public string[,] GetData()
+        {
+            string[,] Data = { };
+            int j = 0;
+            int size = dataGridView1.RowCount;
+            for (int i = 0; i < size - 1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[2].Value != null && !(bool)dataGridView1.Rows[i].Cells[2].Value)
+                {
+                    Data[j, 0] = (string)dataGridView1.Rows[i].Cells[0].Value;
+                    Data[j, 1] = (string)dataGridView1.Rows[i].Cells[1].Value;
+                }
+            }
+            return Data;
+        }
+
+        
+
+        // Figure out how wide each column should be.
+        private int[] FindColumnWidths(Graphics gr, Font header_font,
+            Font body_font, string[] headers, string[,] values)
+        {
+            // Make room for the widths.
+            int[] widths = new int[headers.Length];
+
+            // Find the width for each column.
+            for (int col = 0; col < widths.Length; col++)
+            {
+                // Check the column header.
+                widths[col] = (int)gr.MeasureString(
+                    headers[col], header_font).Width;
+
+                // Check the items.
+                for (int row = 0; row <= values.GetUpperBound(0); row++)
+                {
+                    int value_width = (int)gr.MeasureString(
+                        values[row, col], body_font).Width;
+                    if (widths[col] < value_width)
+                        widths[col] = value_width;
+                }
+
+                // Add some extra space.
+                widths[col] += 20;
+            }
+
+            return widths;
+        }
+    }
+}
